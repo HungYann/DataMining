0b8edd57617a29df684d073509849049106f22cd
LIU-HONGYANG <1664698982@qq.com>
2020-03-13 15:12:42 +0800

    milestone1

--- Milestone1/oilCrawl/oilCrawl.py
+++ Milestone1/oilCrawl/oilCrawl.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 26 16:47:52 2020
 
-@author: Gunasegarran
-"""
+#liuhongyang
 
 import requests
 import requests.cookies
@@ -13,13 +10,23 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
 url='https://www.macrotrends.net/2516/wti-crude-oil-prices-10-year-daily-chart'
-res = requests.get(url)
 
-df = pd.read_html(res.text)
-pan = pd.DataFrame(df[0])
-d={'Year':pan.iloc[:,0],'AverageClosing Price':pan.iloc[:,1],'Year Open':pan.iloc[:,2],'Year High':pan.iloc[:,3],'Year Low':pan.iloc[:,4],'Year Close':pan.iloc[:,5],'Annual% Change':pan.iloc[:,6]}
-df = pd.DataFrame(data=d)
-df.to_csv('oilCrawl.csv')
-df
+headers={
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'
+}
+
+res = requests.get(url,headers=headers)
+
+
+try:
+
+    df = pd.read_html(res.text)
+    pan = pd.DataFrame(df[0])
+    d={'Year':pan.iloc[:,0],'AverageClosing Price':pan.iloc[:,1],'Year Open':pan.iloc[:,2],'Year High':pan.iloc[:,3],'Year Low':pan.iloc[:,4],'Year Close':pan.iloc[:,5],'Annual% Change':pan.iloc[:,6]}
+    df = pd.DataFrame(data=d)
+    df.to_csv('oilCrawl.csv')
+
+except:
+
+    print("The url doesn't work!")
